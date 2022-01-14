69747e10c925206469262c724b5fa91080c244c5
menthus123 <MarvelJohnson@Hotmail.com>
2022-01-14 09:36:55 -0800

    Setup one-to-many association between Routine and ScheduledWorkout

--- models/routine.js
+++ models/routine.js
@@ -9,6 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Routine.hasMany(models.ScheduledWorkout, {
+        as: 'scheduled_workouts',
+        foreignKey: 'routine_id'
+      });
     }
   }
   Routine.init(
