98f0aa2d923c4680cc2d735174c48630fa2cea24
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2022-01-14 18:22:42 -0500

    added css and onCLick to side bar

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Nav from './Nav'
 import Main from './Main'
 import Burger from '../assets/burger.svg'
 
 function Home() {
+    const [sideBAr, SetSideBar] = useState("min")
+    function click(s){
+        if (s == "min") {
+            SetSideBar("max")
+        }else{
+            SetSideBar("min")
+        }
+    }
     return (
         <div className='home'>
-            <div className="sideBar max">
-                <div id="burger" className='add'><img src={Burger} alt="" /></div>
+            <div className={`sideBar ${sideBAr}`}>
+                <div id="burger" className='add' onClick={()=>{click(sideBAr)}}><img src={Burger} alt="" /></div>
                 <div id='side-content'>
                 </div>
             </div>
