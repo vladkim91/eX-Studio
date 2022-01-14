0906fbdb19540395a4d014e91232149ad07d3b45
menthus123 <MarvelJohnson@Hotmail.com>
2022-01-14 12:39:05 -0800

    Updated User_Exercises migration

--- migrations/20220114200056-create-user-exercise.js
+++ migrations/20220114200056-create-user-exercise.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('User_Exercises', {
+    await queryInterface.createTable('user_exercises', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -9,10 +9,20 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       user_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        allowNull: false
       },
       exercise_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'exercises',
+          key: 'id'
+        },
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
@@ -25,6 +35,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('User_Exercises');
+    await queryInterface.dropTable('user_exercises');
   }
 };
