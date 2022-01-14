94b3a10dd754dbab991e584038756b3dc801e99f
menthus123 <MarvelJohnson@Hotmail.com>
2022-01-14 08:48:44 -0800

    Updated exercise migration

--- migrations/20220114164252-create-exercise.js
+++ migrations/20220114164252-create-exercise.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Exercises', {
+    await queryInterface.createTable('exercises', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Exercises');
+    await queryInterface.dropTable('exercises');
   }
 };
