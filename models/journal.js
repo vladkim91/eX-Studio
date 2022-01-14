35c3dc5b3ae2bd1f3fcdd2b61b45f2800e89ec45
menthus123 <MarvelJohnson@Hotmail.com>
2022-01-14 09:00:17 -0800

    Setup one-to-one association between Journal and User

--- models/journal.js
+++ models/journal.js
@@ -9,6 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Journal.belongsTo(models.User, {
+        as: 'journal'
+      });
     }
   }
   Journal.init(
