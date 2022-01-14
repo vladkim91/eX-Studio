72b52e3a354ee0ce1f75b1c117d88b9c0c5e5849
menthus123 <MarvelJohnson@Hotmail.com>
2022-01-14 09:40:57 -0800

    Setup one-to-one association between ScheduledWorkout and Workout

--- models/scheduledworkout.js
+++ models/scheduledworkout.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       ScheduledWorkout.belongsTo(models.Routine);
+      ScheduledWorkout.hasOne(models.Workout);
     }
   }
   ScheduledWorkout.init(
