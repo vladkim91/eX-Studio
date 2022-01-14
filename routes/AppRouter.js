0270917022a87a8a3d70b489340df6ca891e10b0
vladkim91 <vkcoding21@gmail.com>
2022-01-14 08:06:11 -0800

    dependecies installed & directory structure created

---
+++ routes/AppRouter.js
@@ -0,0 +1,10 @@
+const Router = require('express').Router();
+// const LiegeRouter = require('./LiegeRouter');
+// const CastleRouter = require('./CastleRouter');
+// const VesselRouter = require('./VesselRouter');
+// Router.use('/lieges', LiegeRouter);
+
+// Router.use('/vessels', VesselRouter);
+
+// Router.use('/castles', CastleRouter);
+module.exports = Router;
