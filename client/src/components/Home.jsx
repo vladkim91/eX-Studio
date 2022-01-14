0c34ee201b5e63452eb6cd06607bc5dfc20f19a8
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2022-01-14 18:09:06 -0500

    set side bar

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Nav from './Nav'
 import Main from './Main'
+import Burger from '../assets/burger.svg'
 
 function Home() {
     return (
-        <div>
-            <div className="sideBar">
-                
+        <div className='home'>
+            <div className="sideBar max">
+                <div id="burger" className='add'><img src={Burger} alt="" /></div>
+                <div id='side-content'>
+                </div>
             </div>
             <div className="mainBody">
                 <Nav />
