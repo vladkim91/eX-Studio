e7e40f52dfb75248732bf581522fe97a8fa40e3a
menthus123 <MarvelJohnson@Hotmail.com>
2022-01-14 18:51:36 -0800

    Removed unneeded routes from Routine

--- routes/routine.js
+++ routes/routine.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 const controller = require('../controllers/routine');
 
-router.post('/', controller.createRoutineForUser);
 router.get('/:userId', controller.getRoutineByUser);
-router.put('/:routineId/:userId', controller.updateRoutineByUser);
-router.delete('/:routineId/:userId', controller.deleteRoutineByUser);
 
 module.exports = router;
