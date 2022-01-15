4a061301fd4831a0a1cb8d45428b7d6923eaadd1
menthus123 <MarvelJohnson@Hotmail.com>
2022-01-15 13:39:13 -0800

    Setup getWorkoutById function for workout controller

--- controllers/workout.js
+++ controllers/workout.js
@@ -1,1 +1,21 @@
-module.exports = {};
+const { Workout, Exercise } = require('../models');
+
+const getWorkoutById = async (req, res) => {
+  const { workoutId } = req.params;
+  const workout = await Workout({
+    attributes: ['name', 'muscle_groups', 'image'],
+    where: {
+      id: workoutId
+    },
+    include: {
+      model: Exercise,
+      as: 'exerices',
+      attributes: ['name', 'image', 'sets', 'time', 'reps', 'weight']
+    }
+  });
+  res.status(200).send(workout);
+};
+
+module.exports = {
+  getWorkoutById
+};
--- routes/workout.js
+++ routes/workout.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
 const controller = require('../controllers/workout');
 
+router.get('/:workoutId', controller.getWorkoutById);
+
 module.exports = router;
