03fdf7a13fded9bde293ea1b766d2aca201593d4
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2022-01-15 14:30:38 -0500

    finished main layout of routine

--- client/src/components/Routine.jsx
+++ client/src/components/Routine.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import Nav from './Nav'
+import SideBar from './SideBar'
 
 function Routine() {
-    return (
-        <div>
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    
+    let list =[]
+    let j = 1
+    for (let i = 0; i<days.length; i++ ){
+        
+        list.push(<div key={i} className={`routine-cont-list r-${j}`}>
+            <img className='back' src={require(`../assets/img/${days[i]}.jpg`)} alt="" />
+        <div className='blur r-blur'></div>
+        <div className="float">
+        <h1 className="r-name">{days[i]}</h1>
+        </div>
+    </div>)
+    }
+ return (
+        <div className='home'>
+            <SideBar />
+            <div className="mainBody">
+                <Nav/>
+                <div className='routine-list'>
+            <section className="r-list">
+                {list}
+            </section>
             
+        
+                </div>
+            </div>
         </div>
     )
 }
