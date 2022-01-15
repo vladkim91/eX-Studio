3936bcc9146f70fc9adfc0a7cd4812e787c8c102
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2022-01-14 19:12:22 -0500

    completed version .2 of Home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,35 +1,15 @@
 import React, {useState} from 'react'
 import Nav from './Nav'
 import Main from './Main'
-import Burger from '../assets/burger.svg'
-import Closer from '../assets/close.svg'
+
+import SideBar from './SideBar'
 
 function Home() {
-    const [sideBAr, SetSideBar] = useState("min")
-    const [display, SetDisplay] = useState("hide")
-    const [Menu, SetMenu] = useState("Burger")
-    function click(s){
-        if (s == "min") {
-            SetSideBar("max")
-            SetMenu("Closer")
-            setTimeout(()=>{SetDisplay("show")}, 400)
-            
-        }else{
-            SetSideBar("min")
-            SetDisplay("hide")
-            SetMenu("Burger")
-        }
-    }
+    
+    
     return (
         <div className='home'>
-            <div className={`sideBar ${sideBAr}`}>
-                <div id="burger" className='add' onClick={()=>{click(sideBAr)}}><img src={Menu=='Burger'? Burger : Closer} alt="" /></div>
-                <div id='side-content' className={display}>
-                    <li>Browse Exercises</li>
-                    <li>Browse Workouts</li>
-                    <li>Journal</li>
-                </div>
-            </div>
+            <SideBar />
             <div className="mainBody">
                 <Nav />
                 <Main />
