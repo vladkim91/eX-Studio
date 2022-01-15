d216b876e22ca5aa4b3b37b49b762e4ae08301c8
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2022-01-14 19:08:00 -0500

    corrected all the bugs on Home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,21 +2,32 @@ import React, {useState} from 'react'
 import Nav from './Nav'
 import Main from './Main'
 import Burger from '../assets/burger.svg'
+import Closer from '../assets/close.svg'
 
 function Home() {
     const [sideBAr, SetSideBar] = useState("min")
+    const [display, SetDisplay] = useState("hide")
+    const [Menu, SetMenu] = useState("Burger")
     function click(s){
         if (s == "min") {
             SetSideBar("max")
+            SetMenu("Closer")
+            setTimeout(()=>{SetDisplay("show")}, 400)
+            
         }else{
             SetSideBar("min")
+            SetDisplay("hide")
+            SetMenu("Burger")
         }
     }
     return (
         <div className='home'>
             <div className={`sideBar ${sideBAr}`}>
-                <div id="burger" className='add' onClick={()=>{click(sideBAr)}}><img src={Burger} alt="" /></div>
-                <div id='side-content'>
+                <div id="burger" className='add' onClick={()=>{click(sideBAr)}}><img src={Menu=='Burger'? Burger : Closer} alt="" /></div>
+                <div id='side-content' className={display}>
+                    <li>Browse Exercises</li>
+                    <li>Browse Workouts</li>
+                    <li>Journal</li>
                 </div>
             </div>
             <div className="mainBody">
