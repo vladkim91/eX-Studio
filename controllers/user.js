5a9dbb875339fdcb0c6c46e345734022ae0b1e9f
menthus123 <MarvelJohnson@Hotmail.com>
2022-01-15 13:32:45 -0800

    Setup getUserById function in user controller

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,17 @@ const Sequelize = require('sequelize');
 
 const createNewUser = async (req, res) => {};
 
+const getUserById = async (req, res) => {
+  const { userId } = req.params;
+  const user = await User.findOne({
+    attributes: ['first_name', 'last_name'],
+    where: {
+      id: userId
+    }
+  });
+  res.status(200).send(user);
+};
+
 const getUserFavoritedWorkouts = async (req, res) => {
   const { userId } = req.params;
   const favoritedWorkouts = await User.findOne({
@@ -38,6 +49,7 @@ const getUserCustomWorkouts = async (req, res) => {
 
 module.exports = {
   createNewUser,
+  getUserById,
   getUserFavoritedWorkouts,
   getUserCustomWorkouts
 };
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const controller = require('../controllers/user');
 
 router.post('/', controller.createNewUser);
+router.get('/:userId', controller.getUserById);
 router.get('/:userId/favorited_workouts', controller.getUserFavoritedWorkouts);
 router.get('/:userId/custom_workouts', controller.getUserCustomWorkouts);
 
