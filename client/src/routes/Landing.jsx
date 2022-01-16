c665db9e5814ae68d268571cfaf260bbe3a35456
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2022-01-15 20:38:18 -0500

    completed the landingpage version 0.1

--- client/src/routes/Landing.jsx
+++ client/src/routes/Landing.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import LandImg from '../assets/img/landing.jpg'
+import Curve from '../assets/curve.svg'
 
 function Landing() {
     return (
         <div>
             <section className="intro">
-                <img src={LandImg} alt="" />
+                <img className="l-i-back" src={LandImg} alt="" />
                 <header>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Sign In</li>
-                    <li>Sign Up</li>
+                    <Link to={'/'}>Home</Link>
+                    <Link to={'about'}>About</Link>
+                    <Link to={'singin'}>Sign In</Link>
+                    <Link to={'signup'}>Sign Up</Link>
                 </header>
+                <div className="banner">
+                <div className="l-title">
+                    <h1 className='l-t-top'>EX</h1>
+                    <span className='l-t-mid'>/</span>
+                    <h1 className='l-t-bottom'>STUDIO</h1>
+                </div>
+                </div>
+                <img className='curve' src={Curve} alt="" />
             </section>
         </div>
     )
