a328c89a46a9e029410cbb5e94a5a293e244eca9
menthus123 <MarvelJohnson@Hotmail.com>
2022-01-15 16:52:12 -0800

    Added title to note model

--- migrations/20220114164130-create-note.js
+++ migrations/20220114164130-create-note.js
@@ -8,6 +8,10 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
       text: {
         type: Sequelize.STRING
       },
