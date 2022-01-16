cfdf350265c2a978ccae745e0dee829c1d37559b
vladkim91 <vkcoding21@gmail.com>
2022-01-15 20:33:55 -0800

    timer not complete

--- client/src/components/Timer.jsx
+++ client/src/components/Timer.jsx
@@ -3,46 +3,66 @@ import React, { useState, useEffect } from 'react';
 const Timer = ({ fullWorkout }) => {
   const [seconds, setSeconds] = useState(0);
   const [isActive, setisActive] = useState(false);
-  const [timeBased, setTimeBased] = useState(true)
-  const [timers, setTimers] = useState([])
-  const [exCounter, setExCounter] = useState(0)
+  const [timeBased, setTimeBased] = useState(true);
+  const [exCounter, setExCounter] = useState(0);
 
-
-  const timeArray = fullWorkout.map(e => (
-  (e.time) ? {name: e.name, time: e.time} : {name: e.name, reps : e.reps}
-    ))
-  useEffect(() => {
-    setSeconds(timeArray[exCounter].time)
-  }, [])
-  // let timers;
-  function toggle() { 
+  const startWorkout = () => {
+    setSeconds(fullWorkout[exCounter].time)
     setisActive(!isActive);
-    
+
   }
+
+
+
+    
+  
 
   function reset() {
-    setExCounter(count => count + 1)
-    setisActive(false);
+    setExCounter((count) => {
+      let newCount = count +1
+      console.log(fullWorkout[newCount])
+      if (fullWorkout[newCount].time) {
+        setTimeBased(true);
+        setSeconds(fullWorkout[newCount].time)
+        // console.log('timebased',fullWorkout[newCount])
+      } else {
+        // console.log(fullWorkout[newCount])
+        setTimeBased(false);
+      
+      }
+      return newCount
+    });
+    // setisActive(false);
   }
 
   useEffect(() => {
+    console.log('working')
     let interval = null;
 
     if (isActive && timeBased) {
       interval = setInterval(() => {
-        setSeconds((seconds) => seconds - 1);
+        setSeconds((seconds) => {
+          let newSeconds  = seconds - 1
+          if (newSeconds < 1) {
+
+            reset();
+            // clearInterval(interval)
+          }
+          return newSeconds
+        });
+
       }, 1000);
-      if (seconds < 1) {
-       toggle()
-       reset()      
-      }
-    } else if (!isActive && seconds == 0) {
-  
- 
-      clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  }, [isActive, seconds]);
+    } else if (isActive && !timeBased) {
+      interval = setInterval(() => {
+        console.log(seconds)
+        setSeconds((seconds) => seconds + 1);
+      }, 1000);
+      
+    } 
+    return () => {clearInterval(interval)};
+  }, [isActive,timeBased]);
+
+
 
   return (
     <div className="app">
@@ -52,11 +72,15 @@ const Timer = ({ fullWorkout }) => {
           className={`button button-primary button-primary-${
             isActive ? 'active' : 'inactive'
           }`}
-          onClick={toggle}
+          onClick={startWorkout}
         >
           {isActive ? 'Pause' : 'Start'}
         </button>
-        {isActive ? <div>{timeArray[0].name} : {timeArray[0].time}</div> : null}
+        {isActive ? (
+          <div>
+            {fullWorkout[exCounter].name} : {fullWorkout[exCounter].time}
+          </div>
+        ) : null}
       </div>
     </div>
   );
