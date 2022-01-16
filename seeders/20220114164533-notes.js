04b358620cff4d0df9f63e2a3f59ae100ca35572
Mohamed <31968872+Mohamedkone@users.noreply.github.com>
2022-01-15 21:28:09 -0500

    untrackedfiles and added to seed

--- seeders/20220114164533-notes.js
+++ seeders/20220114164533-notes.js
@@ -4,12 +4,14 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     const notes = [
       {
+        title: 'Chest day',
         text: 'Set my PR on bench press today',
         journal_id: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
+        title: 'Chest day',
         text: 'set new  PR on incline bench press',
         journal_id: 1,
         createdAt: new Date(),
