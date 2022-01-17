499f9461bb88ae06758727fe537994be8653a780
menthus123 <MarvelJohnson@Hotmail.com>
2022-01-16 18:08:34 -0800

    Added types to training reducer switch statement

--- client/src/store/reducers/TrainingReducer.js
+++ client/src/store/reducers/TrainingReducer.js
@@ -1,3 +1,10 @@
+import {
+  TRAINING_SET_COUNTDOWN_INTERVAL,
+  TRAINING_SET_COUTNDOWN,
+  TRAINING_SET_CURRENT_EXERCISE_INDEX,
+  TRAINING_SET_WORKOUT_ACTIVE
+} from '../types';
+
 const iState = {
   workout: [],
   timer: {
@@ -10,6 +17,23 @@ const iState = {
 
 const TrainingReducer = (state = iState, action) => {
   switch (action.type) {
+    case TRAINING_SET_COUNTDOWN_INTERVAL:
+      return {
+        ...state,
+        timer: { ...state.timer, countdownInterval: action.payload }
+      };
+    case TRAINING_SET_COUTNDOWN:
+      return { ...state, timer: { ...state.timer, countdown: action.payload } };
+    case TRAINING_SET_CURRENT_EXERCISE_INDEX:
+      return {
+        ...state,
+        timer: { ...state.timer, currentExerciseIndex: action.payload }
+      };
+    case TRAINING_SET_WORKOUT_ACTIVE:
+      return {
+        ...state,
+        timer: { ...state.timer, workoutActive: action.payload }
+      };
     default:
       return { ...state };
   }
