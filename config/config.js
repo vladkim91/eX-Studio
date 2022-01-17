36efe87529e961b3ad6f068ec26825c24afa8b6c
menthus123 <MarvelJohnson@Hotmail.com>
2022-01-17 00:10:57 -0800

    Updated sequelize config

--- config/config.js
+++ config/config.js
@@ -16,7 +16,10 @@ module.exports = {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
-      ssl: true
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
   }
 };
