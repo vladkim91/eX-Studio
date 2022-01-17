39c2a465e60d389ed48e9570e5d1b18c855ff9b0
menthus123 <MarvelJohnson@Hotmail.com>
2022-01-16 18:38:41 -0800

    Added SetFullWorkout to training actions

--- client/src/store/actions/TrainingActions.js
+++ client/src/store/actions/TrainingActions.js
@@ -2,7 +2,8 @@ import {
   TRAINING_SET_COUNTDOWN_INTERVAL,
   TRAINING_SET_COUNTDOWN,
   TRAINING_SET_CURRENT_EXERCISE_INDEX,
-  TRAINING_SET_WORKOUT_ACTIVE
+  TRAINING_SET_WORKOUT_ACTIVE,
+  TRAINING_SET_FULL_WORKOUT
 } from '../types';
 
 export const SetCountdownInterval = (countdownInterval) => {
@@ -30,5 +31,12 @@ export const SetWorkoutActive = (state) => {
   return {
     type: TRAINING_SET_WORKOUT_ACTIVE,
     payload: state
+  };
+};
+
+export const SetFullWorkout = (fullWorkout) => {
+  return {
+    type: TRAINING_SET_FULL_WORKOUT,
+    payload: fullWorkout
   };
 };
