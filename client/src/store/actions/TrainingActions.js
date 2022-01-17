535fe57618fdd50b749b9474aa9519c25408e511
menthus123 <MarvelJohnson@Hotmail.com>
2022-01-16 18:11:21 -0800

    Added actions for training actions

---
+++ client/src/store/actions/TrainingActions.js
@@ -0,0 +1,34 @@
+import {
+  TRAINING_SET_COUNTDOWN_INTERVAL,
+  TRAINING_SET_COUTNDOWN,
+  TRAINING_SET_CURRENT_EXERCISE_INDEX,
+  TRAINING_SET_WORKOUT_ACTIVE
+} from '../types';
+
+export const SetCountdownInterval = (countdownInterval) => {
+  return {
+    type: TRAINING_SET_COUNTDOWN_INTERVAL,
+    payload: countdownInterval
+  };
+};
+
+export const SetCountdown = (countdown) => {
+  return {
+    type: TRAINING_SET_COUTNDOWN,
+    payload: countdown
+  };
+};
+
+export const SetCurrentExerciseIndex = (exerciseIndex) => {
+  return {
+    type: TRAINING_SET_CURRENT_EXERCISE_INDEX,
+    payload: exerciseIndex
+  };
+};
+
+export const SetWorkoutActive = (state) => {
+  return {
+    type: TRAINING_SET_WORKOUT_ACTIVE,
+    payload: state
+  };
+};
