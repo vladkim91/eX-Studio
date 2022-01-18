aa1e50f063605441b2160f215bd0526634b4ce0a
vladkim91 <vkcoding21@gmail.com>
2022-01-17 20:48:39 -0800

    3 workouts added

--- seeders/20220114164435-workout.js
+++ seeders/20220114164435-workout.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     const workouts = [
       {
-        name: 'Chest and Tricep ',
+        name: 'Chest and Tricep',
         muscle_groups: 'ch tr',
         image: `https://static01.nyt.com/images/2017/04/09/well/9minute-workout-promo/9minute-workout-promo-jumbo.png`,
         createdAt: new Date(),
@@ -12,16 +12,72 @@ module.exports = {
         user_id: null
       },
       {
-        name: 'Back workout',
-        muscle_groups: 'ch tr',
+        name: 'Full body sculptor',
+        muscle_groups: 'fb',
         image: `https://static01.nyt.com/images/2017/04/09/well/9minute-workout-promo/9minute-workout-promo-jumbo.png`,
         createdAt: new Date(),
         updatedAt: new Date(),
         user_id: null
       },
       {
-        name: 'Back workout',
-        muscle_groups: 'ch tr',
+        name: 'Back and Bicep',
+        muscle_groups: 'bk bc',
+        image: `https://static01.nyt.com/images/2017/04/09/well/9minute-workout-promo/9minute-workout-promo-jumbo.png`,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        user_id: null
+      },
+      {
+        name: 'Shoulders and Leg day',
+        muscle_groups: 'sh lg',
+        image: `https://static01.nyt.com/images/2017/04/09/well/9minute-workout-promo/9minute-workout-promo-jumbo.png`,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        user_id: null
+      },
+      {
+        name: 'Kettlebell circuit',
+        muscle_groups: 'fb bk sh',
+        image: `https://static01.nyt.com/images/2017/04/09/well/9minute-workout-promo/9minute-workout-promo-jumbo.png`,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        user_id: null
+      },
+      {
+        name: '6 pack builder',
+        muscle_groups: 'ab',
+        image: `https://static01.nyt.com/images/2017/04/09/well/9minute-workout-promo/9minute-workout-promo-jumbo.png`,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        user_id: null
+      },
+      {
+        name: 'Leg day',
+        muscle_groups: 'lg',
+        image: `https://static01.nyt.com/images/2017/04/09/well/9minute-workout-promo/9minute-workout-promo-jumbo.png`,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        user_id: null
+      },
+      {
+        name: 'Big Five',
+        muscle_groups: 'ch lg sh bk',
+        image: `https://static01.nyt.com/images/2017/04/09/well/9minute-workout-promo/9minute-workout-promo-jumbo.png`,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        user_id: null
+      },
+      {
+        name: 'Straight outta USSR',
+        muscle_groups: 'bk lg fb',
+        image: `https://static01.nyt.com/images/2017/04/09/well/9minute-workout-promo/9minute-workout-promo-jumbo.png`,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        user_id: null
+      },
+      {
+        name: 'Vertical jump essentials',
+        muscle_groups: 'lg fb',
         image: `https://static01.nyt.com/images/2017/04/09/well/9minute-workout-promo/9minute-workout-promo-jumbo.png`,
         createdAt: new Date(),
         updatedAt: new Date(),
--- seeders/20220117193351-workout-exercise.js
+++ seeders/20220117193351-workout-exercise.js
@@ -2,6 +2,96 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const workout_exercises = [
+      // Chest and Tricep
+      {
+        workout_id: 1,
+        exercise_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 1,
+        exercise_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 1,
+        exercise_id: 4,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 1,
+        exercise_id: 6,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      // Full body
+      {
+        workout_id: 2,
+        exercise_id: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 2,
+        exercise_id: 19,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 2,
+        exercise_id: 7,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 2,
+        exercise_id: 32,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 2,
+        exercise_id: 28,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      // Back and Bicep
+      {
+        workout_id: 3,
+        exercise_id: 8,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 3,
+        exercise_id: 14,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 3,
+        exercise_id: 11,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 3,
+        exercise_id: 17,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 3,
+        exercise_id: 15,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ];
+    await queryInterface.bulkInsert('workout_exercises', workout_exercises);
     /**
      * Add seed commands here.
      *
@@ -10,10 +100,11 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
   },
 
   down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('workout_exercises', workout_exercises);
     /**
      * Add commands to revert seed here.
      *
