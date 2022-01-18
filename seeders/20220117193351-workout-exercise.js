fa96799be2bbf68c04c42439b6c9087e25339a55
vladkim91 <vkcoding21@gmail.com>
2022-01-18 07:56:09 -0800

    10 workouts complete

--- seeders/20220117193351-workout-exercise.js
+++ seeders/20220117193351-workout-exercise.js
@@ -89,6 +89,200 @@ module.exports = {
         exercise_id: 15,
         createdAt: new Date(),
         updatedAt: new Date()
+      },
+      // Shoulders and leg day
+      {
+        workout_id: 4,
+        exercise_id: 23,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 4,
+        exercise_id: 27,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 4,
+        exercise_id: 26,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 4,
+        exercise_id: 29,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 4,
+        exercise_id: 30,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      // Kettbell circuit
+      {
+        workout_id: 5,
+        exercise_id: 32,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 5,
+        exercise_id: 9,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 5,
+        exercise_id: 25,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 5,
+        exercise_id: 31,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      // 6 pack builder
+      {
+        workout_id: 6,
+        exercise_id: 18,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 6,
+        exercise_id: 21,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 6,
+        exercise_id: 19,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 6,
+        exercise_id: 22,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      // Leg day
+      {
+        workout_id: 7,
+        exercise_id: 28,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 7,
+        exercise_id: 29,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 7,
+        exercise_id: 27,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 7,
+        exercise_id: 30,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      // Big 5
+      {
+        workout_id: 8,
+        exercise_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 8,
+        exercise_id: 27,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 8,
+        exercise_id: 8,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 8,
+        exercise_id: 24,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 8,
+        exercise_id: 11,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      // Straight outta USSE
+
+      {
+        workout_id: 9,
+        exercise_id: 9,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 9,
+        exercise_id: 34,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 9,
+        exercise_id: 31,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 9,
+        exercise_id: 31,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      // Vertical jump essintials
+      {
+        workout_id: 10,
+        exercise_id: 30,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 10,
+        exercise_id: 29,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 10,
+        exercise_id: 31,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 10,
+        exercise_id: 33,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        workout_id: 10,
+        exercise_id: 34,
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ];
     await queryInterface.bulkInsert('workout_exercises', workout_exercises);
