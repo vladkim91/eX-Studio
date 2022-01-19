7eb0f5bd237bca106a7a2abe16a0f862121751ff
menthus123 <MarvelJohnson@Hotmail.com>
2022-01-18 17:22:34 -0800

    Removed unneeded dependency

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -4,18 +4,13 @@ const routine = require('./routine');
 const user = require('./user');
 const workout = require('./workout');
 const google = require('../google_api');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 router.use('/journal', journal);
 router.use('/routine', routine);
 router.use('/user', user);
 router.use('/workout', workout);
-router.get(
-  '/authtest',
-  createProxyMiddleware({
-    target: google,
-    changeOrigin: true
-  })
-);
+router.get('/authtest', (req, res) => {
+  res.redirect(google);
+});
 
 module.exports = router;
