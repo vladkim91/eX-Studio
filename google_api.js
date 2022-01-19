fd3bcaf433f66741369f32097ebc7195ae9f30e6
menthus123 <MarvelJohnson@Hotmail.com>
2022-01-18 17:27:00 -0800

    Updated oauth redirect uri to plain string

--- google_api.js
+++ google_api.js
@@ -4,7 +4,7 @@ const { google } = require('googleapis');
 const oauth2Client = new google.auth.OAuth2(
   process.env.GCID,
   process.env.GCS,
-  process.env.GRURI
+  'https://ex-studio.herokuapp.com/'
 );
 
 google.options({ auth: oauth2Client });
