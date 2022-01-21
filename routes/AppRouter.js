c64dd36394ac6da868d3887f0daefc53c8a0b465
menthus123 <MarvelJohnson@Hotmail.com>
2022-01-20 17:06:10 -0800

    Added account route and updated authentication middleware function name

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -4,16 +4,13 @@ const routine = require('./routine');
 const user = require('./user');
 const workout = require('./workout');
 const exercise = require('./exercise');
-const { authenticateMiddleware } = require('../middleware/auth');
+const account = require('./account');
 
 router.use('/journal', journal);
 router.use('/routine', routine);
 router.use('/user', user);
 router.use('/workout', workout);
-router.post('/googleOAuth', authenticateMiddleware, async (req, res) => {
-  console.log(req.payload);
-  res.send({ message: 'Success!' });
-});
+router.post('/account', authenticateMiddleware, account);
 router.use('/exercise', exercise);
 
 module.exports = router;
